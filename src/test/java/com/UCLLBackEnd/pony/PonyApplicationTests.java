e7f8f4bc957ccb2041e857dcb952a59360036ae2
JashanpreetSinghUCLL <jashanpreet.singh@student.ucll.be>
2024-06-13 15:42:34 +0200

    pony

---
+++ src/test/java/com/UCLLBackEnd/pony/PonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UCLLBackEnd.pony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
